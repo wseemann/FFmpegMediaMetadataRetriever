93dbc50b97acb2d55d7ea9d850e35382b26d04e1
William Seemann <seemann@AITSUL3SEEMANN.(none)>
2013-04-04 22:27:29 -0500

    Initial check in

---
+++ library/jni/metadata/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ffmpeg_mediametadataretriever_jni
+LOCAL_CFLAGS := 
+LOCAL_SRC_FILES := MediaMetadataRetriever.cpp \
+	mediametadataretriever.cpp \
+        ffmpeg_mediametadataretriever.c
+LOCAL_SHARED_LIBRARIES := libavcodec libavformat libavutil
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/include
+LOCAL_LDLIBS := -llog
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ library/jni/metadata/mediametadataretriever.cpp
@@ -0,0 +1,61 @@
+/*
+ * FFmpegMediaMetadataRetriever: A unified interface for retrieving frame 
+ * and meta data from an input media file.
+ *
+ * Copyright 2013 William Seemann
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//#define LOG_NDEBUG 0
+#define LOG_TAG "MediaMetadataRetriever"
+
+#include <mediametadataretriever.h>
+
+extern "C" {
+	#include "libavcodec/avcodec.h"
+	#include "libavformat/avformat.h"
+	#include "ffmpeg_mediametadataretriever.h"
+}
+
+using namespace std;
+
+MediaMetadataRetriever::MediaMetadataRetriever()
+{
+	state = NULL;
+}
+
+MediaMetadataRetriever::~MediaMetadataRetriever()
+{
+	::release(&state);
+}
+
+int MediaMetadataRetriever::setDataSource(const char *srcUrl)
+{
+	return ::set_data_source(&state, srcUrl);
+}
+
+AVPacket* MediaMetadataRetriever::getFrameAtTime(int64_t timeUs)
+{
+    return ::get_frame_at_time(&state, timeUs);
+}
+
+const char* MediaMetadataRetriever::extractMetadata(const char *key)
+{
+    return ::extract_metadata(&state, key);
+}
+
+AVPacket* MediaMetadataRetriever::extractAlbumArt()
+{
+    return ::get_embedded_picture(&state);
+}
