a365297394bf50200e45ec7a2580a18511eca531
William Seemann <wseemann@Williams-MacBook-Pro.local>
2013-08-02 01:19:52 -0500

    Remove unneeded files

--- ffmpegmediametadataretriever/gen/wseemann/media/ffmpegmediametadataretriever/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package wseemann.media.ffmpegmediametadataretriever;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
