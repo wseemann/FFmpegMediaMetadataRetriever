e2af7c9cb6d2222fcda7daf95a45db74bcbbf40b
William Seemann <wseemann@gmail.com>
2015-01-15 14:36:33 -0600

    Add filesize metadata key

--- fmmr-demo/src/wseemann/media/demo/Constants.java
+++ fmmr-demo/src/wseemann/media/demo/Constants.java
@@ -50,7 +50,8 @@ public class Constants {
 		FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_CODEC,
 		FFmpegMediaMetadataRetriever.METADATA_KEY_ICY_METADATA,
 		FFmpegMediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION,
-		FFmpegMediaMetadataRetriever.METADATA_KEY_FRAMERATE
+		FFmpegMediaMetadataRetriever.METADATA_KEY_FRAMERATE,
+		FFmpegMediaMetadataRetriever.METADATA_KEY_FILESIZE
 	};
 	
 }
