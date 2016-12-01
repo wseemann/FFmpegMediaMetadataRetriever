ece22bc99b3b7a22330997204d353856a79befde
William Seemann <wseemann@gmail.com>
2016-11-30 23:39:36 -0600

    Add mutex support

--- gradle/fmmr-library/library/src/main/jni/metadata/mediametadataretriever.cpp
+++ gradle/fmmr-library/library/src/main/jni/metadata/mediametadataretriever.cpp
@@ -33,50 +33,60 @@ MediaMetadataRetriever::MediaMetadataRetriever()
 
 MediaMetadataRetriever::~MediaMetadataRetriever()
 {
+	Mutex::Autolock _l(mLock);
 	::release(&state);
 }
 
 int MediaMetadataRetriever::setDataSource(const char *srcUrl, const char *headers)
 {
+	Mutex::Autolock _l(mLock);
 	return ::set_data_source_uri(&state, srcUrl, headers);
 }
 
 int MediaMetadataRetriever::setDataSource(int fd, int64_t offset, int64_t length)
 {
+	Mutex::Autolock _l(mLock);
     return ::set_data_source_fd(&state, fd, offset, length);
 }
 
 int MediaMetadataRetriever::getFrameAtTime(int64_t timeUs, int option, AVPacket *pkt)
 {
+	Mutex::Autolock _l(mLock);
     return ::get_frame_at_time(&state, timeUs, option, pkt);
 }
 
 int MediaMetadataRetriever::getScaledFrameAtTime(int64_t timeUs, int option, AVPacket *pkt, int width, int height)
 {
+	Mutex::Autolock _l(mLock);
     return ::get_scaled_frame_at_time(&state, timeUs, option, pkt, width, height);
 }
 
 const char* MediaMetadataRetriever::extractMetadata(const char *key)
 {
+	Mutex::Autolock _l(mLock);
     return ::extract_metadata(&state, key);
 }
 
 const char* MediaMetadataRetriever::extractMetadataFromChapter(const char *key, int chapter)
 {
+	Mutex::Autolock _l(mLock);
     return ::extract_metadata_from_chapter(&state, key, chapter);
 }
 
 int MediaMetadataRetriever::extractAlbumArt(AVPacket *pkt)
 {
+	Mutex::Autolock _l(mLock);
     return ::get_embedded_picture(&state, pkt);
 }
 
 int MediaMetadataRetriever::getMetadata(bool update_only, bool apply_filter, AVDictionary **metadata)
 {
+	Mutex::Autolock _l(mLock);
     return get_metadata(&state, metadata);
 }
 
 int MediaMetadataRetriever::setNativeWindow(ANativeWindow* native_window)
 {
+    Mutex::Autolock _l(mLock);
 	return ::set_native_window(&state, native_window);
 }
--- gradle/fmmr-library/library/src/main/jni/metadata/mediametadataretriever.h
+++ gradle/fmmr-library/library/src/main/jni/metadata/mediametadataretriever.h
@@ -21,6 +21,7 @@
 #define MEDIAMETADATARETRIEVER_H
 
 #include <android/native_window_jni.h>
+#include <Mutex.h>
 
 extern "C" {
     #include "ffmpeg_mediametadataretriever.h"
@@ -42,6 +43,9 @@ public:
     const char* extractMetadataFromChapter(const char* key, int chapter);
     int getMetadata(bool update_only, bool apply_filter, AVDictionary **metadata);
     int setNativeWindow(ANativeWindow* native_window);
+
+private:
+    Mutex                                     mLock;
 };
 
 #endif // MEDIAMETADATARETRIEVER_H
