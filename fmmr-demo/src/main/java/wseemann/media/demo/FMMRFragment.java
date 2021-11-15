04bfb4177c6e3cf9a0e8bf1240376d53281b8597
wseemann <wseemann@gmail.com>
2021-11-14 21:46:58 -0800

    Migrate from gradle.plugin.com.vanniktech:gradle-maven-publish-plugin:0.8.0 to maven-publish plugin

--- fmmr-demo/src/main/java/wseemann/media/demo/FMMRFragment.java
+++ fmmr-demo/src/main/java/wseemann/media/demo/FMMRFragment.java
@@ -83,7 +83,7 @@ public class FMMRFragment extends ListFragment
     	
     	final EditText uriText = (EditText) v.findViewById(R.id.uri);
     	// Uncomment for debugging
-		uriText.setText("https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+		uriText.setText("https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4");
     	// uriText.setText("http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_abl.mp4");
 		// https://gist.github.com/jsturgis/3b19447b304616f18657
 
