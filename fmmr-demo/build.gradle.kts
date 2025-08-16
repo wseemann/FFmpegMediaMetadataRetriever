bd0001309cac496b34c3141527d79449b9affd0f
wseemann <wseemann@gmail.com>
2025-08-16 08:58:09 -0700

    Clean up dependencies

--- fmmr-demo/build.gradle.kts
+++ fmmr-demo/build.gradle.kts
@@ -63,9 +63,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
     testImplementation(libs.junit)
-
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
